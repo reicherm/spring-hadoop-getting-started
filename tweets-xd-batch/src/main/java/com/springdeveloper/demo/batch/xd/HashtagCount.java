0e15da458b2866d7fe76c2aed0fa255e6f0dca8c
Thomas Risberg <trisberg@gopivotal.com>
2014-08-26 21:37:27 -0400

    Updating XD Batch Job example

--- tweets-xd-batch/src/main/java/com/springdeveloper/demo/batch/xd/HashtagCount.java
+++ tweets-xd-batch/src/main/java/com/springdeveloper/demo/batch/xd/HashtagCount.java
@@ -78,23 +78,4 @@ public class HashtagCount {
 		}
 	}
 
-	public static void main(String[] args) throws Exception {
-		Configuration conf = new Configuration();
-		String[] otherArgs = new GenericOptionsParser(conf, args)
-				.getRemainingArgs();
-		if (otherArgs.length != 2) {
-			System.err.println("Usage: wordcount <in> <out>");
-			System.exit(2);
-		}
-		Job job = new Job(conf, "hashtag count");
-		job.setJarByClass(HashtagCount.class);
-		job.setMapperClass(TokenizerMapper.class);
-		job.setCombinerClass(LongSumReducer.class);
-		job.setReducerClass(LongSumReducer.class);
-		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(LongWritable.class);
-		FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
-		FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
-		System.exit(job.waitForCompletion(true) ? 0 : 1);
-	}
 }
